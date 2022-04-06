2111d088e15725467ca040b2a91a6bbfe9cc125b
Jean Felder <jean.felder@oslandia.com>
2022-04-06 13:50:06 +0200

    3d: Factor out qgs3dwiredmesh from qgs3daxis
    This will be used by the new bounding box entity.

--- src/3d/qgs3daxis.cpp
+++ src/3d/qgs3daxis.cpp
@@ -31,6 +31,7 @@
 #include <ctime>
 
 #include "qgs3dmapscene.h"
+#include "qgs3dwiredmesh.h"
 #include "qgsterrainentity_p.h"
 #include "qgs3dmapsettings.h"
 #include "qgscoordinatereferencesystemutils.h"
@@ -1011,49 +1012,4 @@ void Qgs3DAxis::setMode( Mode axisMode )
     createAxisScene();
     mParentWindow->requestUpdate();
   }
-}
-
-
-//
-// Qgs3DWiredMesh
-//
-
-Qgs3DWiredMesh::Qgs3DWiredMesh( Qt3DCore::QNode *parent )
-  : Qt3DRender::QGeometryRenderer( parent ),
-    mPositionAttribute( new Qt3DRender::QAttribute( this ) ),
-    mVertexBuffer( new Qt3DRender::QBuffer( this ) )
-{
-  mPositionAttribute->setAttributeType( Qt3DRender::QAttribute::VertexAttribute );
-  mPositionAttribute->setBuffer( mVertexBuffer );
-  mPositionAttribute->setVertexBaseType( Qt3DRender::QAttribute::Float );
-  mPositionAttribute->setVertexSize( 3 );
-  mPositionAttribute->setName( Qt3DRender::QAttribute::defaultPositionAttributeName() );
-
-  mGeom = new Qt3DRender::QGeometry( this );
-  mGeom->addAttribute( mPositionAttribute );
-
-  setInstanceCount( 1 );
-  setIndexOffset( 0 );
-  setFirstInstance( 0 );
-  setPrimitiveType( Qt3DRender::QGeometryRenderer::Lines );
-  setGeometry( mGeom );
-}
-
-Qgs3DWiredMesh::~Qgs3DWiredMesh() = default;
-
-void Qgs3DWiredMesh::setVertices( const QList<QVector3D> &vertices )
-{
-  QByteArray vertexBufferData;
-  vertexBufferData.resize( vertices.size() * 3 * sizeof( float ) );
-  float *rawVertexArray = reinterpret_cast<float *>( vertexBufferData.data() );
-  int idx = 0;
-  for ( const QVector3D &v : std::as_const( vertices ) )
-  {
-    rawVertexArray[idx++] = v.x();
-    rawVertexArray[idx++] = v.y();
-    rawVertexArray[idx++] = v.z();
-  }
-
-  mVertexBuffer->setData( vertexBufferData );
-  setVertexCount( vertices.count() );
 }
