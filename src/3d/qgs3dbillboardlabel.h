ed10985f005e054beb4e24aeba211f0a7b8263b7
Jean Felder <jean.felder@oslandia.com>
2022-06-13 14:25:51 +0200

    qgs3dbillboardlabel: Backport componentsOfType

--- src/3d/qgs3dbillboardlabel.h
+++ src/3d/qgs3dbillboardlabel.h
@@ -1,6 +1,10 @@
 #ifndef QGS3DBILLBOARDLABEL_H
 #define QGS3DBILLBOARDLABEL_H
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+#include <Qt3DCore/QComponent>
+#endif
+
 #include <Qt3DExtras/QText2DEntity>
 #include <QVector3D>
 
@@ -24,6 +28,22 @@ class Qgs3DBillboardLabel: public Qt3DExtras::QText2DEntity
   public:
     Qgs3DBillboardLabel( const QgsCameraController *cameraController, Qt3DCore::QNode *parent = nullptr );
     void setTranslation( const QVector3D &position );
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
+    template<class T>
+    QVector<T *> componentsOfType() const
+    {
+      QVector<T *> matchComponents;
+      const QVector<Qt3DCore::QComponent *> comps = this->components();
+      for ( Qt3DCore::QComponent *component : comps )
+      {
+        T *typedComponent = qobject_cast<T *>( component );
+        if ( typedComponent != nullptr )
+          matchComponents.append( typedComponent );
+      }
+      return matchComponents;
+    }
+#endif
 
   private slots:
     void onCameraViewChanged();
